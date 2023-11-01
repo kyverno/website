8e2e528d3b0e82b1fd07e8c48a569d2468e3f1d3
Chip Zoller <chipzoller@gmail.com>
2023-11-01 13:08:40 +0000

    remove duplicate hugo
    Signed-off-by: Chip Zoller <chipzoller@gmail.com>

--- .devcontainer/tools.sh
+++ .devcontainer/tools.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 ## Install Hugo Extended v0.119.0
-wget https://github.com/gohugoio/hugo/releases/download/v0.119.0/hugo_extended_0.119.0_linux-amd64.tar.gz -O hugo.tar.gz && tar -xzf hugo.tar.gz hugo && sudo mv hugo /usr/bin/hugo && rm hugo.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v0.119.0/hugo_extended_0.119.0_linux-amd64.tar.gz -O hugo.tar.gz && tar -xzf hugo.tar.gz hugo && sudo mv hugo /usr/local/bin/hugo && rm hugo.tar.gz && sudo rm -rf /usr/local/hugo/
