5cea4b403a6976cc12bfcfcf783821d7f70bb578
Luc Chmielowski <luc.chmielowski@nirmata.com>
2025-10-29 12:18:30 +0100

    feat: add netlify + update ci
    Signed-off-by: Luc Chmielowski <luc.chmielowski@nirmata.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,35 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import netlify from '@astrojs/netlify'
+import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+  integrations: [
+    starlight({
+      title: 'Kyverno',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/kyverno/kyverno',
+        },
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+
+  adapter: netlify(),
+})
