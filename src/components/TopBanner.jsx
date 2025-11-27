2239c07ceb03046557d8b4a470730f40f0c29543
Dev-Liz <basseyelizabeth569@gmail.com>
2025-11-27 21:31:21 +0100

    fixed responsiveness for all screens
    Signed-off-by: Dev-Liz <basseyelizabeth569@gmail.com>

--- src/components/TopBanner.jsx
+++ src/components/TopBanner.jsx
@@ -3,7 +3,7 @@ import { Star, X } from 'lucide-react';
 
 export const TopBanner = ({onClose}) => {
   return (
-    <div className='w-full top-0 bg-accent-100 text-[1rem] flex justify-center items-center p-2 space-x-6'>
+    <div className='text-sm w-full top-0 bg-accent-100 flex justify-center items-center p-2 space-x-3 md:space-x-6 md:text-lg'>
         <div className='flex justify-center items-center space-x-2'>
                 <Star className='fill-amber-400 stroke-1 stroke-gray-800'/>
                 <span>
@@ -13,7 +13,7 @@ export const TopBanner = ({onClose}) => {
                 </span>
                 <Star className='fill-amber-400 stroke-1 stroke-gray-800'/>
         </div>
-        <button onClick={onClose} className='border border-dark-50 cursor-pointer'>
+        <button onClick={onClose} className='border border-dark-50 cursor-pointer flex justify-center items-center'>
             < X className='w-5 h-5' />
         </button>
     </div>
--- src/components/YamlVsCelCard.jsx
+++ src/components/YamlVsCelCard.jsx
@@ -2,7 +2,7 @@ import { twMerge } from "tailwind-merge";
 
 export const YamlVsCelCard = ({card, color}) => {
   return (
-    <div className="w-[80%] overflow-x-clip sm:w-1/3 md:w-1/2 bg-dark-50 rounded-2xl sm:h-130">
+    <div className="w-[80%] overflow-x-clip sm:w-1/3 lg:w-1/2 bg-dark-50 rounded-2xl sm:h-130">
         <div className={twMerge("p-4 text-sm md:text-[16px] text-center text-dark-100 rounded-tl-2xl rounded-tr-2xl flex items-center justify-start space-x-2", color.bg)}>
              <card.icon className="w-4 h-4"/>
              <span className="font-bold">{card.title}</span>
