ff9c0771b116fad3673106a6c9ed0a2d57eb6e3d
Luc Chmielowski <lucchmielowski@users.noreply.github.com>
2025-12-02 18:31:30 +0900

    chore: Prettier format (#1726)
    Signed-off-by: Luc Chmielowski <luc.chmielowski@nirmata.com>

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,41 +1,50 @@
-import { Github, Slack, Twitter, Mail } from "lucide-react"
-import { communityLinks, policies, productsLinks, ResourcesLinks } from "../constants"
-import { SiteLogo } from "../components/SiteLogo";
-import { BottomNavLinks } from "../components/BottomNavLinks";
-import { BottomLinkLists } from "../components/BottomLinkLists";
-
+import { Github, Slack, Twitter, Mail } from 'lucide-react'
+import {
+  communityLinks,
+  policies,
+  productsLinks,
+  ResourcesLinks,
+} from '../constants'
+import { SiteLogo } from '../components/SiteLogo'
+import { BottomNavLinks } from '../components/BottomNavLinks'
+import { BottomLinkLists } from '../components/BottomLinkLists'
 
 export const Footer = () => {
-
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear()
 
   return (
     <footer className="flex flex-col items-start bg-dark-footer">
-        <div className="w-full flex flex-col  p-12 space-y-6 md:space-y-0 md:flex-row md:justify-evenly text-[16px]">
-            <div className="1/4 flex flex-col items-start space-y-4">
-                <SiteLogo />
-                <p className="max-w-80 text-white/80">Unified Policy as Code for Platform Engineers. Secure, automate, and govern your infrastructure with next-generation CEL-based policies.</p>
-                <span className="flex justify-center items-center space-x-2 cursor-pointer">
-                    <Github />
-                    <Slack />
-                    <Twitter />
-                    <Mail />
-                </span>
-            </div>
-            <BottomLinkLists links={productsLinks} title="Products"/>
-            <BottomLinkLists links={ResourcesLinks} title="Resources"/>
-            <BottomLinkLists links={communityLinks} title="Community"/>
+      <div className="w-full flex flex-col  p-12 space-y-6 md:space-y-0 md:flex-row md:justify-evenly text-[16px]">
+        <div className="1/4 flex flex-col items-start space-y-4">
+          <SiteLogo />
+          <p className="max-w-80 text-white/80">
+            Unified Policy as Code for Platform Engineers. Secure, automate, and
+            govern your infrastructure with next-generation CEL-based policies.
+          </p>
+          <span className="flex justify-center items-center space-x-2 cursor-pointer">
+            <Github />
+            <Slack />
+            <Twitter />
+            <Mail />
+          </span>
         </div>
-        <div className="w-full space-y-4 flex flex-col p-12 justify-between items-start md:items-center 
-        border-t border-stroke text-white/80">
-            <ul className="flex items-center justify-between space-x-3">
-                 {policies.map(({text, href}) => (
-                        <li key={text} className="hover:text-accent-100"><a href={href}>{text}</a></li>
-                ))}
-            </ul>
-            <p>&copy; {currentYear} Kyverno created by Nirmata.</p>
-        </div>
+        <BottomLinkLists links={productsLinks} title="Products" />
+        <BottomLinkLists links={ResourcesLinks} title="Resources" />
+        <BottomLinkLists links={communityLinks} title="Community" />
+      </div>
+      <div
+        className="w-full space-y-4 flex flex-col p-12 justify-between items-start md:items-center 
+        border-t border-stroke text-white/80"
+      >
+        <ul className="flex items-center justify-between space-x-3">
+          {policies.map(({ text, href }) => (
+            <li key={text} className="hover:text-accent-100">
+              <a href={href}>{text}</a>
+            </li>
+          ))}
+        </ul>
+        <p>&copy; {currentYear} Kyverno created by Nirmata.</p>
+      </div>
     </footer>
   )
 }
-
