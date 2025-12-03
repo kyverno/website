7788752288edb530117596f15e8df3ba37a48501
Dev-Liz <basseyelizabeth569@gmail.com>
2025-12-03 13:13:11 +0100

    added prettier formatting
    Signed-off-by: Dev-Liz <basseyelizabeth569@gmail.com>

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -6,8 +6,7 @@ import { motion } from 'motion/react'
 import { HeadingContent } from '../components/HeadingContent'
 
 export const HeroSection = () => {
-  
-  const zapIcon = Zap;
+  const zapIcon = Zap
 
   const { headingText, paragraphText } = heroSectionHeadingContent
 
@@ -18,10 +17,15 @@ export const HeroSection = () => {
         alt="lg-hero-ball"
         className="
             hidden sm:inline-block absolute sm:top-20 sm:left-8 lg:top-20 
-            lg:left-20"/>
-            <img src="assets/images/sm-hero-ball.svg" alt="sm-hero-ball" className="hidden
+            lg:left-20"
+      />
+      <img
+        src="assets/images/sm-hero-ball.svg"
+        alt="sm-hero-ball"
+        className="hidden
             sm:inline-block absolute sm:right-15 sm:top-50 md:right-25
-            lg:top-50 lg:right-50 xl:right-90 "/>
+            lg:top-50 lg:right-50 xl:right-90 "
+      />
 
       <div
         className="container py-10 sm:py-0 sm:px-10 flex flex-col justify-center 
@@ -58,6 +62,15 @@ export const HeroSection = () => {
             Explore Kyverno
           </Button>
         </div>
+        <div className="flex flex-col space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+          <Button href="/docs/quickstart" variant="primary" size="large">
+            Quickstart
+          </Button>
+          <Button href="/docs/introduction" variant="secondary" size="large">
+            Read Documentation
+          </Button>
+        </div>
+      </div>
     </section>
   )
 }
