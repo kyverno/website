3bc200195b5dd12351e66381a80ee6b346e8ab7a
Dev-Liz <basseyelizabeth569@gmail.com>
2025-12-18 15:51:33 +0100

    fixed broken images on landing page
    Signed-off-by: Dev-Liz <basseyelizabeth569@gmail.com>

--- src/components/SiteLogo.jsx
+++ src/components/SiteLogo.jsx
@@ -1,11 +1,7 @@
 export const SiteLogo = () => {
   return (
     <a href="/" className="flex space-x-3 hover:text-accent-100 cursor-pointer">
-      <img
-        src="/public/kyverno-logo.svg"
-        alt="kyverno logo"
-        className="w-8 h-8"
-      />
+      <img src="/kyverno-logo.svg" alt="kyverno logo" className="w-8 h-8" />
       <span className="text-xl tracking-tight font-bold">Kyverno</span>
     </a>
   )
--- src/sections/CncfSection.jsx
+++ src/sections/CncfSection.jsx
@@ -4,11 +4,7 @@ export const CncfSection = () => {
       <h2 className="text-2xl font-bold text-primary-100">
         Kyverno is a CNCF Incubating Project
       </h2>
-      <img
-        src="/public/assets/images/cncf.png"
-        alt="cncf logo"
-        className="w-120"
-      />
+      <img src="/assets/images/cncf.png" alt="cncf logo" className="w-120" />
       <p className="max-w-100">
         The Linux Foundation® (TLF) has registered trademarks and uses
         trademarks. For a list of TLF trademarks, see Trademark Usage.
