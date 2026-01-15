166a64ff4f5f6f06d8eab1decf8e8621ec313756
Luc Chmielowski <luc.chmielowski@nirmata.com>
2026-01-15 12:02:07 +0100

    fix missing public assets
    Signed-off-by: Luc Chmielowski <luc.chmielowski@nirmata.com>

--- src/components/SiteLogo.jsx
+++ src/components/SiteLogo.jsx
@@ -1,7 +1,11 @@
 export const SiteLogo = () => {
   return (
     <a href="/" className="flex space-x-3 hover:text-accent-100 cursor-pointer">
-      <img src="/kyverno-logo.svg" alt="kyverno logo" className="w-8 h-8" />
+      <img
+        src="/assets/images/kyverno-logo.svg"
+        alt="kyverno logo"
+        className="w-8 h-8"
+      />
       <span className="text-xl tracking-tight font-bold">Kyverno</span>
     </a>
   )
